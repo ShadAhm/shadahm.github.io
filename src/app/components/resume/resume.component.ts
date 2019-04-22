490e70e806907d0fbd10b52736919c97f40b23ca
Shad <arsh90@gmail.com>
2019-04-22 16:39:33 +0800

    resume page mock

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ResumeContent } from 'src/app/models/resume';
 
 @Component({
   selector: 'app-resume',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resume.component.sass']
 })
 export class ResumeComponent implements OnInit {
+  contents: ResumeContent[]; 
 
   constructor() { }
 
   ngOnInit() {
+    this.getResumeContents();
   }
 
+  scrollIntoView(elementId : string) : void {
+    document.getElementById(elementId).scrollIntoView({behavior: 'smooth'}); 
+  }
+
+  getResumeContents() : void {
+    this.contents = [
+      {
+        displayText : 'Introduction',
+        elementId : 'h2Introduction'
+      },      
+      {
+        displayText : 'Technical Skills',
+        elementId : 'h2Skills'
+      },      
+      {
+        displayText : 'Employment History',
+        elementId : 'h2Employment'
+      },
+      {
+        displayText : 'Education and Certifications',
+        elementId : 'h2Education'
+      }];
+  }
 }
---
+++ src/app/models/resume.ts
@@ -0,0 +1,5 @@
+export class ResumeContent 
+{
+    public displayText : string;
+    public elementId : string; 
+}
